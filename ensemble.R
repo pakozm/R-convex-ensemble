7187ce3f8925495db792a2fb6215c7caadc292de
Paco Zamora Martinez <pakozm@gmail.com>
2015-08-25 10:14:30 +0200

    minor fix

--- ensemble.R
+++ ensemble.R
@@ -14,9 +14,9 @@ ensemble <- function(val_matrix, val_target, test_matrix, loss) {
     x0 <- matrix(ncol(val_matrix), 1, data=0.0)
     result <- fminsearch(loss_ensemble, x0, val_matrix, val_target, dfree=TRUE)
     wp <- as.matrix(result$xval)
-    print(wp)
     print(c("VAL_LOSS",result$fval))
     w <- softmax(wp)
+    print(t(w))
     test_p <- test_matrix %*% w
     test_p
 }
