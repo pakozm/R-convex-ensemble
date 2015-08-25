30a3c32fdcf084611ab34477689d0976ef289044
Paco Zamora Martinez <pakozm@gmail.com>
2015-08-25 10:11:38 +0200

    Updated

--- ensemble.R
+++ ensemble.R
@@ -1,27 +1,21 @@
-train <- read.csv("DATA/train.csv")
-train <- train[sample(nrow(train)),]
-target <- train$target
-f1 <- read.csv(“model1.csv”)
-f2 <- read.csv(“model2.csv”)
-f <- rbind(f1,f2)
-logloss <- function(outputs,target) {
-  p <- max(min(outputs, 1 - 10e-15), 10e-15))
-  -sum(target * log(p))/nrows(p)
-}
-softmax <- function(m) {
-  exp(m)/sum(exp(m))
-}
-logloss_ensemble <- function(wp) {
-  w <- softmax(wp)
-  p <- T(w) %*% f
-  logloss(p, target)
-}
-x0 <- matrix(1,cols(f), 0.0)
-wp <- fminsearch(logloss_ensemble, x0)
-w <- softmax(wp)
-test_f1 <- read.csv(“model1_test.csv”)
-test_f2 <- read.csv(“model2_test.csv”)
-test_f <- rbind(test_f1,test_f2)
-test_p <- T(w) %*% f
-write.csv(test_p, file = "result.csv", row.names=FALSE, na="")
+library(pracma)
 
+softmax <- function(m) {
+    s <- exp(m)/sum(exp(m))
+    s
+}
+loss_ensemble <- function(wp, val_matrix, val_target) {
+    w <- softmax(wp)
+    p <- val_matrix %*% w
+    e <- loss(p, val_target)
+    e
+}
+ensemble <- function(val_matrix, val_target, test_matrix, loss) {
+    x0 <- matrix(ncol(val_matrix), 1, data=0.0)
+    result <- fminsearch(loss_ensemble, x0, val_matrix, val_target, dfree=TRUE)
+    wp <- as.matrix(result$xval)
+    print(c("VAL_LOSS",result$fval))
+    w <- softmax(wp)
+    test_p <- test_matrix %*% w
+    test_p
+}
